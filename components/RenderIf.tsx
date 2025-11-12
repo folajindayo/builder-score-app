916b93c5463fb04cc72193ea198d14b07d172561
folajindayo <temidayofolajin@yahoo.com>
2025-11-12 17:43:01 +0100

    feat: add RenderIf component

---
+++ components/RenderIf.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ConditionalRender } from "@/components/ConditionalRender";
+import { ReactNode } from "react";
+
+interface RenderIfProps {
+  condition: boolean;
+  children: ReactNode;
+}
+
+export function RenderIf({
+  condition,
+  children,
+}: RenderIfProps) {
+  return (
+    <ConditionalRender condition={condition}>
+      {children}
+    </ConditionalRender>
+  );
+}
+
