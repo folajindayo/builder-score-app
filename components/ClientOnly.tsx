4223559ea6c537a9772e89ea11163b0f97f42ec0
folajindayo <temidayofolajin@yahoo.com>
2025-11-12 17:42:36 +0100

    feat: add ClientOnly component

---
+++ components/ClientOnly.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { NoSSR } from "@/components/NoSSR";
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+  fallback?: React.ReactNode;
+}
+
+export function ClientOnly({
+  children,
+  fallback,
+}: ClientOnlyProps) {
+  return (
+    <NoSSR fallback={fallback}>
+      {children}
+    </NoSSR>
+  );
+}
+
