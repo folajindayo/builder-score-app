eb7bece98eb8b9b551f6fddcea83d87f93cc2aed
folajindayo <temidayofolajin@yahoo.com>
2025-11-12 15:43:17 +0100

    feat: add debounce utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -155,6 +155,27 @@ export function throttle<T extends (...args: any[]) => any>(
 }
 
 /**
+ * Debounces a function to delay its execution until after a specified time has passed
+ * @param func - The function to debounce
+ * @param delay - The delay in milliseconds
+ * @returns A debounced version of the function
+ */
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: NodeJS.Timeout | null = null;
+  return (...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+/**
  * Parses a URL and returns its components
  * @param url - The URL to parse
  * @returns An object with URL components or null if invalid
