8d2089a197e46d220f2035910dbfda38933f73f7
folajindayo <temidayofolajin@yahoo.com>
2025-11-12 17:43:14 +0100

    feat: add Show component

---
+++ components/Show.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { RenderIf } from "@/components/RenderIf";
+import { ReactNode } from "react";
+
+interface ShowProps {
+  when: boolean;
+  children: ReactNode;
+  fallback?: ReactNode;
+}
+
+export function Show({
+  when,
+  children,
+  fallback,
+}: ShowProps) {
+  return (
+    <RenderIf condition={when}>
+      {children}
+    </RenderIf>
+  );
+}
+
