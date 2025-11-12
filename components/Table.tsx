6f9ad9c6fe859de139c3e48f65f6cb4069cb49a1
folajindayo <temidayofolajin@yahoo.com>
2025-11-12 17:33:39 +0100

    feat: add Table component

---
+++ components/Table.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface TableProps {
+  children: ReactNode;
+  striped?: boolean;
+  hover?: boolean;
+  className?: string;
+}
+
+export function Table({
+  children,
+  striped = false,
+  hover = false,
+  className = "",
+}: TableProps) {
+  return (
+    <div className="overflow-x-auto">
+      <table
+        className={`w-full border-collapse ${striped ? "[&_tbody_tr:nth-child(even)]:bg-gray-50" : ""} ${hover ? "[&_tbody_tr:hover]:bg-gray-100" : ""} ${className}`}
+      >
+        {children}
+      </table>
+    </div>
+  );
+}
+
