04994e6d10c88b3af3b008e90d660a02370de39a
folajindayo <temidayofolajin@yahoo.com>
2025-11-13 14:35:35 +0100

    refactor: update organisms exports with all components

--- src/presentation/components/organisms/index.ts
+++ src/presentation/components/organisms/index.ts
@@ -3,12 +3,16 @@
  * Export all organism components
  */
 
+export * from './Accordion';
 export * from './Card';
 export * from './DataTable';
 export * from './DropdownMenu';
+export * from './FileUpload';
 export * from './Form';
 export * from './Modal';
 export * from './Navbar';
 export * from './Pagination';
 export * from './Sidebar';
+export * from './Stepper';
 export * from './Tabs';
+export * from './Timeline';
