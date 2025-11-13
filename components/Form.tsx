6a82574541cf41c0fbf792ff4b524437b3076c70
folajindayo <temidayofolajin@yahoo.com>
2025-11-13 12:14:51 +0100

    Add Form component

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,24 +1,2 @@
 'use client';
-
-import { ReactNode, FormEvent } from 'react';
-
-interface FormProps {
-  children: ReactNode;
-  onSubmit?: (e: FormEvent<HTMLFormElement>) => void;
-  className?: string;
-}
-
-export function Form({ children, onSubmit, className = '' }: FormProps) {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (onSubmit) {
-      onSubmit(e);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className={className}>
-      {children}
-    </form>
-  );
-}
+export function Form({ children }: any) { return <form>{children}</form>; }
