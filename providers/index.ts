25f28628c605cbbe3d70c86b99f68cd9673d4dfc
folajindayo <temidayofolajin@yahoo.com>
2025-11-19 19:02:29 +0100

    Add providers barrel export for cleaner imports

---
+++ providers/index.ts
@@ -0,0 +1,5 @@
+export { AppProviders } from "./AppProviders";
+export { ScoreProvider, useScore } from "@/contexts/ScoreContext";
+export { ThemeProvider, useTheme } from "@/contexts/ThemeContext";
+export { LeaderboardProvider, useLeaderboardContext } from "@/contexts/LeaderboardContext";
+
