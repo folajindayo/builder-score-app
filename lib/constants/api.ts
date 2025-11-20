4229559de4b5540e8a30c9867eff66ca304f5b3d
folajindayo <temidayofolajin@yahoo.com>
2025-11-20 22:36:01 +0100

    feat(builder-score): add API constants

---
+++ lib/constants/api.ts
@@ -0,0 +1,17 @@
+/**
+ * API Constants
+ */
+
+export const TALENT_API_BASE_URL = 'https://api.talentprotocol.com/v1';
+
+export const API_ENDPOINTS = {
+  BUILDER: '/builders',
+  SEARCH: '/search',
+  CREDENTIALS: '/credentials',
+};
+
+export const API_CONFIG = {
+  TIMEOUT: 30000,
+  RETRY_COUNT: 3,
+};
+
