644cd7d21bd9418965dcff825f2a6d7cb6ed0efe
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 13:03:20 +0100

    chore: add services barrel export

---
+++ src/application/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Services Barrel Export
+ */
+
+export { leaderboardService } from './leaderboard.service';
+export { githubService } from './github.service';
+export { talentProtocolService } from './talent-protocol.service';
+
